8eaf1f29ceec92c1d2bfdaba6f5afa42dcd1fe90
islam rwashdeh <57620910+islamrwashdeh@users.noreply.github.com>
2022-09-04 01:47:51 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+
+function App() {
+  return (
+    <div className="w-screen h-screen flex flex-col justify-between items-center bg-rose-900">
+      <Header />
+      <Hero />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
